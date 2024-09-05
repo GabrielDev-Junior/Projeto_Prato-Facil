64767aabb9459bcacc1508b1e3d49a87e9c28c5c
Gabriel Avelino <gabrielavelinoamorim@gmail.com>
2024-09-05 14:10:33 -0300

    closes #8 - Ajustar Layout pag Index.php com CSS

--- index.php
+++ index.php
@@ -10,43 +10,47 @@
 </head>
 
 <body>
- <header>
-  <nav>
-   <ul>
-    <li><a href="pages/login.php">Entrar</a></li>
-    <li><a href="pages/signup.php">Cadastrar</a></li>
-    <li><a href="pages/sobre.php">Sobre</a></li>
-   </ul>
-  </nav>
- </header>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="pages/login.php">Entrar</a></li>
+        <li><a href="pages/signup.php">Cadastrar</a></li>
+        <li><a href="pages/sobre.php">Sobre</a></li>
+      </ul>
+    </nav>
+  </header>
 
  <main>
   <div class="intro">
-   <h1>Prato Fácil</h1>
-   <p>Ache a receita perfeita com os ingredientes que você tem.</p>
-   <a href="pages/home.php" class="btn">Ver Receitas</a>
-  </div>
+    <h1>Prato Fácil</h1>
+    <p>Ache a receita perfeita com os ingredientes que você tem.</p>
+    <a href="pages/home.php" class="btn">Ver Receitas</a>
+  </div>      
  </main>
 
  <footer>
-  <div class="footer-content">
-   <div class="footer-links">
-    <a href="privacy.php">Política de Privacidade</a>
-    <a href="terms.php">Termos de Serviço</a>
-    <a href="contact.php">Contato</a>
-    <a href="help.php">Ajuda</a>
-   </div>
-   <div class="footer-social">
-    <a href="https://www.facebook.com" target="_blank">Facebook</a> |
-    <a href="https://www.instagram.com" target="_blank">Instagram</a> |
-    <a href="https://www.twitter.com" target="_blank">Twitter</a>
-   </div>
-   <div class="footer-logo">
-    <img src="assets/images/logo-footer.png" alt="Logo Prato Fácil">
-   </div>
-   <div class="footer-copyright">
-    &copy; 2024 Prato Fácil. Todos os direitos reservados.
-   </div>
+  <div class= "footer">
+    <div class="sub-footer">
+      <div class="footer-content">
+        <div class="footer-links">
+          <a href="privacy.php">Política de Privacidade</a>
+          <a href="terms.php">Termos de Serviço</a>
+          <a href="contact.php">Contato</a>
+          <a href="help.php">Ajuda</a>
+        </div>
+        <div class="footer-social">
+          <a href="https://www.facebook.com" target="_blank">Facebook</a> |
+          <a href="https://www.instagram.com" target="_blank">Instagram</a> |
+          <a href="https://www.twitter.com" target="_blank">Twitter</a>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        &copy; 2024 Prato Fácil. Todos os direitos reservados.
+      </div>
+    </div>
+    <div class="footer-logo">
+      <img src="assets/images/prato.png" alt="Logo Prato Fácil">
+    </div>
   </div>
  </footer>
 
